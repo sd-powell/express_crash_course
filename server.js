0247bfcbf06933ed53db1d906b64f8f5b3d722db
sd-powell <kf210rhd@students.codeinstitute.net>
2025-11-12 20:58:32 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// Import the Express library
+const express = require('express');
+
+// Create an instance of an Express application
+const app = express();
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+    });
+
+app.get('/about', (req, res) => {
+    res.send('About');
+    });
+
+// Start the server and listen on port 5000
+app.listen(5000, () => console.log('Server is running on port 8000'));
+
+// watch flag is enabled for development convenience
+// npm run dev
