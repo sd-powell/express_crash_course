305a902258ebba5807262b0013bf8f5da96f83bf
sd-powell <kf210rhd@students.codeinstitute.net>
2025-11-12 21:50:55 +0000

    complete status codes section

--- server.js
+++ server.js
@@ -34,17 +34,24 @@ app.get('/api/posts', (req, res) => {
     const limit = parseInt(req.query.limit);
 
     if (!isNaN(limit) && limit > 0) {
-        // Returns 200 by default
-        res.json(posts.slice(0, limit));
-    } else {    
-        res.json(posts);
+        // Returns 200 by default - but added here for clarity
+        return res.status(200).json(posts.slice(0, limit));
     }
+    
+    res.status(200).json(posts);
 });
 
 // Get a single post by ID
 app.get('/api/posts/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    res.json(posts.filter((post) => post.id === id));
+    // res.json(posts.filter((post) => post.id === id));
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return res.status(404).json({ msg: `A post with the id of ${id} was not found`});
+    }
+
+    res.status(200).json(post);
 });
 
 
