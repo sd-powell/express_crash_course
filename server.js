628225b9c585b1523c69436d9b2093b06e1604ad
sd-powell <kf210rhd@students.codeinstitute.net>
2025-11-12 21:05:46 +0000

    complete static webserver section

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 // Import the Express library
 const express = require('express');
+// Import the path module
+const path = require('path');
 
 // Create an instance of an Express application
 const app = express();
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
 
 app.get('/about', (req, res) => {
-    res.send('About');
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
     });
 
 // Start the server and listen on port 5000
-app.listen(5000, () => console.log('Server is running on port 8000'));
+app.listen(8000, () => console.log('Server is running on port 8000'));
 
 // watch flag is enabled for development convenience
 // npm run dev
