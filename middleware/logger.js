ac48ccbe66edbb2a67a34cb22953faaf34e613f1
sd-powell <kf210rhd@students.codeinstitute.net>
2025-11-13 17:07:00 +0000

    complete colors section

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,21 @@
+import colors from 'colors';
+
 // Middleware logger
 const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    const methodColors = {
+        GET: 'green',
+        POST: 'blue',
+        PUT: 'yellow',
+        DELETE: 'red',
+        PATCH: 'magenta',
+    };
+
+    const color = methodColors[req.method] || 'white';
+
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`[
+    color
+]
+);
     next();
 };
 
