9ce261058f561bd2e70c9cf1e9f44a3a934c1f20
sd-powell <kf210rhd@students.codeinstitute.net>
2025-11-13 15:54:29 +0000

    complete PUT post section

--- routes/posts.js
+++ routes/posts.js
@@ -52,6 +52,19 @@ router.post('/', (req, res) => {
     res.status(201).json({ msg: 'Post created' });
 });
 
+// Update a post by ID
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    
+    if (!post) {
+        return res.status(404).json({ msg: `A post with the id of ${id} was not found`});
+    }
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+});
+
 // Export the router
 // module.exports = router;
 export default router;
